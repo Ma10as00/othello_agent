7533f1a5b92aa4bc30e15b3d77e05a899a4bc37d
Tobias Verheijen <tobiasverheijen@lclark.edu>
2024-05-23 16:19:37 +0200

    Load trained models from saved networks

---
+++ load_testing.py
@@ -0,0 +1,31 @@
+import copy
+import numpy as np
+import torch
+from Board import Board
+import utils
+
+
+board_size = 4
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+file_name = f'{board_size}x{board_size}_model.pth'
+trained_model = utils.load_trained_network(file_name, device)
+
+# board = Board(board_size=board_size)
+# state = utils.get_state(board)
+# action_board = utils.generate_action_board(board_size)
+#
+# legal_actions = utils.get_legal_actions(board, player=1, action_board=action_board)
+#
+# action_q_values = utils.apply_filter(trained_model(torch.tensor(state).to(device)), legal_actions)
+#
+# action = torch.argmax(action_q_values).detach().cpu().numpy()
+#
+# x_val, y_val = np.where(action_board == action)
+# state_post_move, num_flip = board.make_move(x_val[0], y_val[0], player=1)
+#
+# board.set_board(copy.deepcopy(state_post_move))
+#
+# print(num_flip)
+#
+# board.print_board()
